aed828851545df060b92cd1769d66eaddb746931
lubum593 <luis.mb593@gmail.com>
2025-05-10 17:10:20 +0100

    2024283 Luis Medina Burgoa

--- src/InputUtilities/InputUtilities.java
+++ src/InputUtilities/InputUtilities.java
@@ -13,30 +13,30 @@ import java.util.Scanner;
  */
 public class InputUtilities {
     Scanner sc = new Scanner(System.in);
-    
+    //Method to insert a name with validation
     public String insertName(String prompt){
         String name="";
         System.out.print(prompt);
         do{
             name = sc.nextLine();
-            if (!name.matches("[a-zA-Z]+")||(name.length()<3)) {
+            if (!name.matches("[a-zA-Z]+")||(name.length()<3)) {//name lenght accepted at least 3 letters
                 System.out.println("Name format incorrect, please try again.");
                 System.out.print(prompt);
             }
         }while(!name.matches("[a-zA-Z]+")||(name.length()<3));
         return name;
     }
-    
+    //Method to insert a number with validation
     public int insertNumber(String prompt){
         System.out.print(prompt);
         String number = sc.nextLine();
         while(true){
             if (!number.matches("[0-9]+")) {
-                if (!number.matches("")) {
+                if (!number.matches("")) {//No empty values allow
                     System.out.println("You must enter valid numbers only");
                     System.out.print(prompt);
                 }
-            }else if(Integer.parseInt(number)>0){
+            }else if(Integer.parseInt(number)>0){//only positive numbers allow
                 return (Integer.parseInt(number));
             }else{
                 System.out.println("You must enter valid numbers only");
