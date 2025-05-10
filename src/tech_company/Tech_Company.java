bb3f3beedf95761f50d553c895a72fcbaae86795
lubum593 <luis.mb593@gmail.com>
2025-05-10 16:10:46 +0100

    2024283 Luis Medina Burgoa

--- src/tech_company/Tech_Company.java
+++ src/tech_company/Tech_Company.java
@@ -26,7 +26,7 @@ public class Tech_Company {
     static InputUtilities input = new InputUtilities();
     static int ID = 0;
     static List<Employee> employee = new ArrayList<>();
-    private static final String fileName = "Applicants_Form.txt";
+    private static String fileName;
     
     /**
      * @param args the command line arguments
@@ -34,10 +34,11 @@ public class Tech_Company {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        fileName = "Applicants_Form.txt";//Name of the file
         loadFromFile(fileName);//Load our database stored in our .txt File
-        mainMenu();
+        mainMenu();//Ititiate the program
     }
-        
+    //Main menu with the list
     public static void mainMenu(){
         MenuOptions selectedMenu;
         do{
@@ -63,9 +64,9 @@ public class Tech_Company {
                 }
                 case EXIT -> System.out.println("Exiting...");
             }
-        }while(selectedMenu != MenuOptions.EXIT);
+        }while(selectedMenu != MenuOptions.EXIT);//Finish the program only if the user enters the case EXIT
     }
-    
+    //Method to add employees
     private static void addEmployee() {
         System.out.println("\n-- Adding new Employee --");
         String name = input.insertName("Enter Employee First Name: ");
@@ -74,47 +75,11 @@ public class Tech_Company {
         int compensationChoice = CompensationOptions.select().getCode();
         int departmentChoice = DepartmentOptions.select().getCode();
         int EntityChoice = EntityOptions.select().getCode();
-        ID++;
-        createEmployee(name, surname, employeeChoice, compensationChoice, departmentChoice, EntityChoice, false);
+        ID++;//will increase the employee ID
+        createEmployee(name, surname, employeeChoice, compensationChoice, departmentChoice, EntityChoice, false);//call the method, sending the necessary data
     }
-    
-    private static void createEmployee(String name, String surname, int employeeChoice, int compensationChoice, int departmentChoice, int EntityChoice, boolean loadFiles){
-        Employee newEmployee = null;
-        Department newDepartment = null;
-        Compensation newCompensation = null;
-        Entity newEntity = null;
-        String employeeOption = EmployeeOptions.fromCode(employeeChoice).getDescription();
-        String compensationOption = CompensationOptions.fromCode(compensationChoice).getDescription();//MODIFY
-        String departmentOption = DepartmentOptions.fromCode(departmentChoice).getDescription();
-        String entityOption = EntityOptions.fromCode(EntityChoice).getDescription();//MODIFY
-        switch(compensationChoice){
-                case 1 -> newCompensation = new FullTimeEmployee(compensationOption);
-                case 2 -> newCompensation = new PartTimeEmployee(compensationOption);
-                case 3 -> newCompensation = new ContractorEmployee(compensationOption);
-        }
-        switch(departmentChoice){
-                case 1 -> newDepartment = new CustomerService(departmentOption);
-                case 2 ->  newDepartment = new TechnicalSupport(departmentOption);
-                case 3 ->  newDepartment = new HumanResources(departmentOption);
-        }
-        switch(EntityChoice){
-                case 1 -> newEntity = new EntityIreland(entityOption);
-                case 2 -> newEntity = new EntitySpain(entityOption);
-                case 3 -> newEntity = new EntityLondon(entityOption);
-                case 4 -> newEntity = new EntityUSA(entityOption);
-        }
-        switch(employeeChoice){
-                case 1 -> newEmployee = new Developer(ID, name, surname, employeeOption, newCompensation, newDepartment, newEntity);
-                case 2 ->  newEmployee = new Tester(ID, name, surname, employeeOption, newCompensation, newDepartment, newEntity);
-                case 3 ->  newEmployee = new Designer(ID, name, surname, employeeOption, newCompensation, newDepartment, newEntity);
-        }
-        employee.add(newEmployee);
-        if (!loadFiles) {//To avoid printing the list when the system load from the .txt file
-            System.out.println("\n"+ID+": "+name + " " + surname + " has been added as " + newEmployee.getPosition()+ " with a "+ newCompensation.getTypeOfEmployee() + " contract to " + newDepartment + " successfully!\n");
-            writeToFile(newEmployee, fileName);
-        }
-    }
-    
+            
+    //Method to generate a random employee
     private static void generateEmployee(){
         System.out.println("\n-- Generate Employees --");
         int numberEmployees = input.insertNumber("How many employees want to generate: ");
@@ -122,19 +87,57 @@ public class Tech_Company {
         String[] surnames = {"Grey", "Stone", "Dane", "Smith", "Brown", "Lee", "Garcia", "Davis", "Miller", "Hawk", "Shaw", "Silva"};
         for (int i = 0; i < numberEmployees; i++) {
             Random randomEmployee = new Random();
-            String name = names[randomEmployee.nextInt(names.length)];
-            String surname = surnames[randomEmployee.nextInt(surnames.length)];
-            EntityOptions entityOption = EntityOptions.values()[randomEmployee.nextInt(EntityOptions.values().length)];//We get a random value from the enum of employeeOptions
+            String name = names[randomEmployee.nextInt(names.length)];//generate a random name from the array name
+            String surname = surnames[randomEmployee.nextInt(surnames.length)];//generate a random surname from the array surname
             int employeeChoice = EmployeeOptions.values()[randomEmployee.nextInt(EmployeeOptions.values().length)].getCode();//We get a random value from the enum of employeeOptions
             int compensationChoice = CompensationOptions.values()[randomEmployee.nextInt(CompensationOptions.values().length)].getCode();//We get a random value from the enum of CompensationOptions
             int departmentChoice = DepartmentOptions.values()[randomEmployee.nextInt(DepartmentOptions.values().length)].getCode();//We get a random value from the enum of DepartmentOptions
             int entityChoice = EntityOptions.values()[randomEmployee.nextInt(EntityOptions.values().length)].getCode();//We get a random value from the enum of EntityOptions
-            ID++;
-            createEmployee(name, surname, employeeChoice, compensationChoice, departmentChoice, entityChoice, false);
+            ID++;//will increase the employee ID
+            createEmployee(name, surname, employeeChoice, compensationChoice, departmentChoice, entityChoice, false);//call the method, sending the necessary data
         }
         System.out.println(numberEmployees + " employee(s) were generated\n");
     }
     
+    //Method where generate employees, adding employees create a new employee.
+    private static void createEmployee(String name, String surname, int employeeChoice, int compensationChoice, int departmentChoice, int EntityChoice, boolean loadFiles){
+        Employee newEmployee = null;
+        Department newDepartment = null;
+        Compensation newCompensation = null;
+        Entity newEntity = null;
+        String employeeOption = EmployeeOptions.fromCode(employeeChoice).getDescription();//To get what type of employee is (Dev, Tester, Designer)
+        String compensationOption = CompensationOptions.fromCode(compensationChoice).getDescription();//To get if the employee works (Full time, Part Time)
+        String departmentOption = DepartmentOptions.fromCode(departmentChoice).getDescription();//To get the department name
+        String entityOption = EntityOptions.fromCode(EntityChoice).getDescription();//To get the entity name
+        switch(compensationChoice){//Choosing one compensation
+                case 1 -> newCompensation = new FullTimeEmployee(compensationOption);
+                case 2 -> newCompensation = new PartTimeEmployee(compensationOption);
+                case 3 -> newCompensation = new ContractorEmployee(compensationOption);
+        }
+        switch(departmentChoice){//Choosing one department
+                case 1 -> newDepartment = new CustomerService(departmentOption);
+                case 2 ->  newDepartment = new TechnicalSupport(departmentOption);
+                case 3 ->  newDepartment = new HumanResources(departmentOption);
+        }
+        switch(EntityChoice){//Choosing an entity
+                case 1 -> newEntity = new EntityIreland(entityOption);
+                case 2 -> newEntity = new EntitySpain(entityOption);
+                case 3 -> newEntity = new EntityLondon(entityOption);
+                case 4 -> newEntity = new EntityUSA(entityOption);
+        }
+        switch(employeeChoice){//Choosing the type of employee
+                case 1 -> newEmployee = new Developer(ID, name, surname, employeeOption, newCompensation, newDepartment, newEntity);
+                case 2 ->  newEmployee = new Tester(ID, name, surname, employeeOption, newCompensation, newDepartment, newEntity);
+                case 3 ->  newEmployee = new Designer(ID, name, surname, employeeOption, newCompensation, newDepartment, newEntity);
+        }
+        employee.add(newEmployee);//adding into the list
+        if (!loadFiles) {//To avoid printing the list AGAIN when the system load from the .txt file
+            System.out.println("\n"+ID+": "+name + " " + surname + " has been added as " + newEmployee.getPosition()+ " with a "+ newCompensation.getTypeOfEmployee() + " contract to " + newDepartment + " successfully!\n");
+            writeToFile(newEmployee, fileName);//Save the new employee into the file .txt
+        }
+    }
+    
+    //Method to append users to the file .txt
     private static void writeToFile(Employee employee, String fileName){
         try(FileWriter writer = new FileWriter(fileName, true)){//true to append
             String name = employee.getName();
@@ -151,6 +154,7 @@ public class Tech_Company {
         }
     }
     
+    //Load data from the .txt file into the system (List employee)
     public static void loadFromFile(String fileName) {
         try(Scanner fileSC = new Scanner(new java.io.File(fileName))){
             System.out.println("File "+fileName+" was read successfully\n");
@@ -184,11 +188,12 @@ public class Tech_Company {
             System.out.println("Error reading the file: "+e.getMessage());
         }
     }
-     
+    
+    //Method to display the list of employees
     public static void displayList(boolean max){
         int maxDisplay;
-        if (max) {
-            maxDisplay = Math.min(20, employee.size());//To display only 20 employees
+        if (max) {//if true the system will print only 20 employees
+            maxDisplay = Math.min(20, employee.size());//To display only 20 employees, and check if the list is smaller than 20
         }else{
             maxDisplay = employee.size();
         }
@@ -199,15 +204,17 @@ public class Tech_Company {
         }
     }
     
+    //Recursive method of Merge Sort
     public static void mergeSort(List<Employee> employeeList, int left, int right){
         if (left<right) {//Only if there is at least one element in the list
-            int middle = left + (right - left)/2;
+            int middle = left + (right - left)/2;//calculate the middle index
             mergeSort(employeeList, left, middle);//it will sort the left half recursively
             mergeSort(employeeList, middle+1, right);//it will sort the right half recursively
-            merge(employeeList, left, middle, right);
+            merge(employeeList, left, middle, right);//then it will sort the two halves
         }
     }
     
+    //Merge method
     public static void merge(List<Employee> employeeList, int left, int middle, int right){
         //creating two variables to get the sizes of each sublist
         int leftSize = middle - left+1;
@@ -222,7 +229,8 @@ public class Tech_Company {
             rightList.add(employeeList.get(middle + 1 + j));
         int i = 0, j=0;
         int index = left;
-        while(i < leftSize && j < rightSize){//Here is where we are going to compare the list
+        //Here is where merge elements based on the name and surname
+        while(i < leftSize && j < rightSize){
             String nameLeft = leftList.get(i).getName();
             String nameRight = rightList.get(j).getName();
             if (nameLeft.compareToIgnoreCase(nameRight)<0) {
@@ -244,12 +252,12 @@ public class Tech_Company {
             }
             index++;
         }
-        //Copying any remain from both lists
+        //Copying any remain from the left lists
         while (i < leftSize) {
             employeeList.set(index, leftList.get(i));
             i++;
             index++;
-        }
+        }//Copying any remain from the right lists
         while (j < rightSize) {
             employeeList.set(index, rightList.get(j));
             j++;
@@ -258,14 +266,16 @@ public class Tech_Company {
     }
     
     private static void binarySearch(List<Employee> list, String name, String surname, int left, int right){
-        if (left > right) {
+        if (left > right) {//if the search inputs are not valid or the employee was not found
             System.out.println("\n-- Result of the search--");
             System.out.println("Employee not found.\n");
             return;
         }
-        int middle = left + (right - left)/2;
+        int middle = left + (right - left)/2; //calculate the middle index
+        //Get the name and surname of the employee
         String employeeName = list.get(middle).getName();
         String employeeSurname = list.get(middle).getSurname().trim();
+        //check if the employee name matches
         if ((employeeName.compareToIgnoreCase(name))==0) {//If the name matches
             if (employeeSurname.compareToIgnoreCase(surname.trim())==0) {//If the lastname matches
                 //return list.get(middle);//The employee was found
@@ -277,10 +287,11 @@ public class Tech_Company {
                 System.out.println("Department: "+list.get(middle).getDepartment().getName());
                 System.out.println("Located in: "+list.get(middle).getEntity().getName()+"\n");
             }
-        }else{
+        }else{//it will call recursively BinarySearch
+            //If the middle name comes after the search nam, the search will go to the Left
             if ((employeeName.compareToIgnoreCase(name))> 0) {
                 binarySearch(list, name, surname, left, middle-1);
-            }else{
+            }else{//If the middle name comes before the search nam, the search will go to the Right
                 binarySearch(list, name, surname, middle+1, right);
             }
         }
